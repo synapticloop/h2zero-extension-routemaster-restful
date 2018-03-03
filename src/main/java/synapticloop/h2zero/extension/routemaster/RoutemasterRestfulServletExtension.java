ab7060db6cb0dd6914c133db8774844dd857d021
synapticloop <contact@synapticloop.com>
2018-03-03 18:59:01 +0000

    start of writing out the routemaster rest properties

--- src/main/java/synapticloop/h2zero/extension/routemaster/RoutemasterRestfulServletExtension.java
+++ src/main/java/synapticloop/h2zero/extension/routemaster/RoutemasterRestfulServletExtension.java
@@ -20,18 +20,35 @@ public class RoutemasterRestfulServletExtension extends Extension {
 	public void generate(JSONObject extensionOptions, Database database, Options options, File outFile, boolean verbose) throws RenderException, ParseException {
 		TemplarContext templarContext = getDefaultTemplarContext(extensionOptions, database, options);
 
-		Parser restServantTemplarParser = getParser("/java-create-routemaster-rest-servant.templar", verbose);
+		
+		writeBaseRestServant(database, options, outFile, verbose, templarContext);
 
+		writeRestServants(database, options, outFile, verbose, templarContext);
+
+		writeRestServantRoutes(database, options, outFile, verbose, templarContext);
+
+	}
+
+	private void writeBaseRestServant(Database database, Options options, File outFile, boolean verbose, TemplarContext templarContext) throws ParseException, RenderException {
 		Parser baseRestServantTemplarParser = getParser("/java-create-routemaster-base-rest-servant.templar", verbose);
 		String pathname = outFile + options.getOutputJava() + database.getPackagePath() + "/routemaster/servant/BaseServant.java";
 		renderToFile(templarContext, baseRestServantTemplarParser, pathname, verbose);
+	}
 
+	private void writeRestServants(Database database, Options options, File outFile, boolean verbose, TemplarContext templarContext) throws RenderException, ParseException {
+		Parser restServantTemplarParser = getParser("/java-create-routemaster-rest-servant.templar", verbose);
 		List<Table> tables = database.getTables();
 		for (Table table : tables) {
 			templarContext.add("table", table);
-			pathname = outFile + options.getOutputJava() + database.getPackagePath() + "/routemaster/servant/" + table.getJavaClassName() + "Servant.java";
+			String pathname = outFile + options.getOutputJava() + database.getPackagePath() + "/routemaster/servant/" + table.getJavaClassName() + "Servant.java";
 			renderToFile(templarContext, restServantTemplarParser, pathname, verbose);
 		}
+	}
+
+	private void writeRestServantRoutes(Database database, Options options, File outFile, boolean verbose, TemplarContext templarContext) throws ParseException, RenderException {
+		Parser baseRestServantTemplarParser = getParser("/java-create-routemaster-rest-servant-routes.templar", verbose);
+		String pathname = outFile + options.getOutputSql() + "routemaster.rest.properties";
+		renderToFile(templarContext, baseRestServantTemplarParser, pathname, verbose);
 	}
 
 }
