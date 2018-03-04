50d5774803b9af4bbbe69333ae53564f094aea4a
synapticloop <contact@synapticloop.com>
2018-03-04 10:46:20 +0000

    updated routes and starting of the server

--- startroutemaster.sh
+++ startroutemaster.sh
@@ -2,6 +2,9 @@
 
 # time to create the routemaster properties file
 
+gradle -b build.h2zero.sqlite3.gradle clean h2zero
+gradle build shadow pTML
+
 cat routemaster.orig.properties > routemaster.properties
 cat src/test/resources/routemaster.rest.properties >> routemaster.properties
 
